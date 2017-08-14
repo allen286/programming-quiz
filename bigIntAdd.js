415289370b4bc148414c71d569ae429653e830e0
allen286 <ascend286@gmail.com>
2017-08-14 17:51:12 +0800

    优化

--- bigIntAdd.js
+++ bigIntAdd.js
@@ -6,32 +6,25 @@
  */
 
 function add(a, b) {
-  // 替换掉字符串开头的0以及字符串中间的空格，如00099->99
+  // 替换掉字符串开头的0以及字符串中间的空格，如0009 99->999
   a = a.replace(/^0+|\s+/g, '');
   b = b.replace(/^0+|\s+/g, '');
 
-  // 保证a是长度较大的那个字符串
-  if (a.length < b.length) {
-    [a, b] = [b, a]
-  }
-
   let result = [];
   let count = 0;
+  let tmp = 0;
 
-  for (let i = 1; i <= a.length; i++) {
-    // 利用~~取整，其中b[负数]===undefined, ~~undefined===0
-    let temp = ~~a[a.length - i] + ~~b[b.length - i < 0] + count;
-
-    temp >= 10 ? [temp, count] = [temp - 10, 1] : count = 0;
-    result.push(temp);
+  for (let i = 1; i <= a.length || i <= b.length || count; i++) {
+    // 利用~~取整，~~undefined===0
+    temp = ~~a[a.length - i] + ~~b[b.length - i] + count;
+    count = temp >= 10;
+    result.unshift(temp % 10);
   }
-  // count>0时才put进去，避免出现多余的0
-  count && result.push(count);
 
-  return result.reverse().join('');
+  return result.join('');
 }
 
 // 测试用例
 console.log(add("0027899", "1"));
-console.log(add("33333333333", "111111111111111"));
+console.log(add("3333333333333333", "7766554433221100"));
 console.log(add('99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999', '1'));
